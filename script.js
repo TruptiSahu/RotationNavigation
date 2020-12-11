c4f8cb0d4153dd069357d659ecf31a628c036e2b
TruptiSahu <truptiranjansahu2@gmail.com>
2020-12-10 16:51:29 -0800

    Done

---
+++ script.js
@@ -0,0 +1,27 @@
+const menuBtnOpen = document.querySelector('.menu-open');
+const menuBtnClose = document.querySelector('.menu-close');
+const circularNavbar = document.querySelector('.circular-navbar');
+const blog = document.querySelector('.blog');
+const navbar = document.querySelector('.navbar');
+
+//Add the active class
+const addActiveClass = (element) => {
+  element.classList.add('active');
+};
+
+//Remove the active class
+const removeActiveClass = (element) => {
+  element.classList.remove('active');
+};
+
+menuBtnOpen.addEventListener('click', () => {
+  addActiveClass(circularNavbar);
+  addActiveClass(blog);
+  addActiveClass(navbar);
+});
+
+menuBtnClose.addEventListener('click', () => {
+  removeActiveClass(circularNavbar);
+  removeActiveClass(blog);
+  removeActiveClass(navbar);
+});
